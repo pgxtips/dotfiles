c03fe0380ff0409d434ccbea4ffe88d4b62454ac
pgxtips <brandongill123@gmail.com>
2025-02-15 03:20:51 +0000

    update: added nvim and updated symlink paths

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,22 +2,22 @@
 echo "Creating symlinks for dotfiles..."
 
 echo "linking .zshrc"
-ln -s $HOME/dotfiles/.zshrc ~/.zshrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 echo "linking i3"
-ln -s $HOME/dotfiles/i3 ~/.config/i3
+ln -s $HOME/dotfiles/i3 $HOME/.config/i3
 
 echo "linking tmux"
-ln -s $HOME/dotfiles/tmux ~/.config/tmux
+ln -s $HOME/dotfiles/tmux $HOME/.config/tmux
 
 echo "linking scripts"
-ln -s $HOME/dotfiles/scripts ~/.config/scripts
+ln -s $HOME/dotfiles/scripts $HOME/.config/scripts
 
 echo "linking alacritty"
-ln -s $HOME/dotfiles/alacritty ~/.config/alacritty
+ln -s $HOME/dotfiles/alacritty $HOME/.config/alacritty
 
 echo "linking nvim"
-ln -s $HOME/dotfiles/nvim ~/.config/nvim
+ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
 
 echo "Finished creating symlinks for dotfiles."
 
